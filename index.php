43885b7490193cc16106316ec1a6808da9d07852
alfredo-valladares <josealfredo.developer@outlook.com>
2020-11-23 02:05:53 -0600

    vendor Hora y mas

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<? 
-echo "Hola Mundo";
+
+    <?php
+
+    require __DIR__."/vendor/autoload.php";
+    use Carbon\Carbon;
+
+    $now = Carbon::now();
+    #echo $now->year."/"; 
+    #echo $now->day;
+    echo $now->hour;
+    echo $now->minute;
+
+
+?>
